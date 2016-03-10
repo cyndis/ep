5c0bdaa1545e623d4294a3c77d355c740e164560
Mikko Perttunen <cyndis@kapsi.fi>
2016-03-10 12:39:55 +0200

    Fix for stable Rust

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-#![feature(old_io, old_path, os, core)]
 #![allow(unused_must_use)]
 
 extern crate epwing;
 extern crate term;
 
 struct Arguments {
-    book_path: Path,
+    book_path: std::path::PathBuf,
     expand_unknown: bool,
-    command: Command
+    command: Command,
+    format: Format
 }
 
 #[derive(Debug)]
@@ -15,33 +15,46 @@ enum Command {
     Search(epwing::subbook::Index, String),
 }
 
+#[derive(Debug, Copy, Clone)]
+enum Format {
+    Terminal,
+    HTML
+}
+
 fn parse_command() -> Option<Arguments> {
+    use std::os::unix::ffi::OsStrExt;
+
     let mut book_path = None;
     let mut query = None;
     let mut expand_unknown = false;
 
-    let args = std::os::args();
-    let mut iter = args.into_iter().skip(1);
+    let args = std::env::args_os();
+    let mut iter = args.skip(1);
+    let mut format = Format::Terminal;
 
     while let Some(arg) = iter.next() {
-        if arg == "-b" {
+        let arg = arg.as_bytes();
+        if arg == b"-b" {
             book_path = Some(iter.next().unwrap());
-        } else if arg == "--expand-unknown-characters" {
+        } else if arg == b"--expand-unknown-characters" {
             expand_unknown = true;
+        } else if arg == b"--html" {
+            format = Format::HTML;
         } else {
-            query = Some(arg);
+            query = Some(std::str::from_utf8(arg).unwrap().to_owned());
         }
     }
 
     let book_path = match book_path {
-        Some(p) => Path::new(p),
-        None    => Path::new(std::env::var("EP_BOOK_PATH").ok().
-                                                           expect("Put book path in $EP_BOOK_PATH"))
+        Some(p) => p.into(),
+        None    => std::env::var("EP_BOOK_PATH").ok().expect("Put book path in $EP_BOOK_PATH")
+                                                .into()
     };
 
     match query {
         Some(query) => {
             Some(Arguments {
+                format: format,
                 book_path: book_path,
                 expand_unknown: expand_unknown,
                 command: Command::Search(epwing::subbook::Index::WordAsIs, query)
@@ -63,11 +76,16 @@ fn convert_custom_character(cp: u16) -> Option<&'static str> {
     }
 }
 
-fn print_text(text: &epwing::subbook::Text, expand_unknown: bool) {
+fn print_text(text: &epwing::subbook::Text, expand_unknown: bool, format: Format) {
     use epwing::subbook::TextElement::{UnicodeString, CustomCharacter, Newline, Indent,
                                        NoNewline, BeginDecoration, EndDecoration, Unsupported};
 
     let mut term = term::stdout().unwrap();
+
+    let nl = match format {
+        Format::Terminal => "\n",
+        Format::HTML => "<br>"
+    };
 
     for elem in text.iter() {
         match *elem {
@@ -79,15 +97,25 @@ fn print_text(text: &epwing::subbook::Text, expand_unknown: bool) {
                     _ => ()
                 }
             },
-            Newline                   => write!(term, "\n").unwrap(),
+            Newline                   => write!(term, "{}", nl).unwrap(),
             Indent(n)                 => {
-                for _ in range(0, n) {
+                for _ in 0..n {
                     (write!(term, " ")).unwrap();
                 }
             },
             NoNewline(_enabled)       => (),
-            BeginDecoration(_deco)    => { term.attr(term::Attr::Standout(true)); },
-            EndDecoration             => { term.reset(); },
+            BeginDecoration(_deco)    => {
+                match format {
+                    Format::Terminal => term.attr(term::Attr::Standout(true)).unwrap(),
+                    Format::HTML => write!(term, "<b>").unwrap()
+                }
+            },
+            EndDecoration             => {
+                match format {
+                    Format::Terminal => term.reset().unwrap(),
+                    Format::HTML => write!(term, "</b>").unwrap()
+                }
+            },
             Unsupported(_tag)         => ()
         }
     }
@@ -122,12 +150,22 @@ Options:
         Command::Search(index, query) => {
             let result = subbook.search(index, &query).unwrap();
             for (i, location) in result.iter().enumerate() {
-                term.attr(term::Attr::Bold);
-                writeln!(term, "-- {} of {} --", i+1, result.len());
-                term.reset();
+                match args.format {
+                    Format::Terminal => {
+                        term.attr(term::Attr::Bold);
+                        writeln!(term, "-- {} of {} --", i+1, result.len());
+                        term.reset();
+                    }
+                    Format::HTML => {
+                        if i > 0 {
+                            writeln!(term, "<hr>");
+                        }
+                        writeln!(term, "<p><b>Entry {} of {}</b></p>", i+1, result.len());
+                    }
+                }
                 let text = subbook.read_text(*location).unwrap();
 
-                print_text(&text, args.expand_unknown);
+                print_text(&text, args.expand_unknown, args.format);
             }
 
             if result.len() == 0 {
